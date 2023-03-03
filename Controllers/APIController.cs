a52075edb5a8ed40707fdf0ada49ad477f1e0ef0
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2023-03-03 14:04:14 +0100

    Add project files.

---
+++ Controllers/APIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_API_Test.Controllers
+{
+    public class APIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
