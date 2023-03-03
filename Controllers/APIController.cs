0ef01eff1dc61f4a79a1d223ed59ac4e73a93200
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2023-03-03 15:03:21 +0100

    Fetching User Data From API & Creating Objects

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_API_Test.Models;
+using System.Net;
+using System.Text.Json;
 
 namespace MVC_API_Test.Controllers
 {
     public class APIController : Controller
     {
+        // INDEX METHOD
         public IActionResult Index()
         {
             return View();
+        }
+
+        // DISPLAY USERS METHOD
+        public IActionResult DisplayUsers()
+        {
+            using (var client = new WebClient())
+            {
+                client.BaseAddress = "https://reqres.in/api/users/";
+                string str = client.DownloadString("");
+
+                var parsed = JsonDocument.Parse(str);
+                string filtered = parsed.RootElement.GetProperty("data").GetRawText();
+
+                var listOfUsers = JsonSerializer.Deserialize<List<APIUser>>(filtered);
+
+                return View(listOfUsers);
+            }
         }
     }
 }
